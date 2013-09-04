83ee5a7a17e82ad953037b0b1573285c57d44cd0
James Runham <james.runham@reedonline.co.uk>
2013-09-04 10:38:15 +0100

    first commit

---
+++ ClearClient/GenericClient.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ClearClient
+{
+    public class GenericClient : IDisposable
+    {
+        private HttpClient client;
+
+        public GenericClient(string baseAddress)
+        {
+            Init(baseAddress);
+        }
+
+        /// <summary>
+        /// set up generic client with basic authentication
+        /// </summary>
+        /// <param name="baseAddress"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        public GenericClient(string baseAddress, string username, string password)
+        {
+            Init(baseAddress);
+            SetBasicAuth(username, password);
+        }
+
+        /// <summary>
+        /// make asynchronous http request and return deserialized object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public T Get<T>(string url)
+        {
+            try
+            {
+                var cleanUrl = CleanUrl(url);
+                var response = client.GetAsync(cleanUrl).Result;
+                var result = JsonConvert.DeserializeObject<T>(response.Content.ReadAsStringAsync().Result);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// make asynchronous http request and return deserialized object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync<T>(string url)
+        {
+            try
+            {
+                var cleanUrl = CleanUrl(url);
+                var response = await client.GetAsync(cleanUrl);
+                var result = JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// make http request and return json
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public string GetJson(string url)
+        {
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var cleanUrl = CleanUrl(url);
+            var response = client.GetAsync(cleanUrl).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        /// <summary>
+        /// make asynchronous http request and return json
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<string> GetJsonAsync(string url)
+        {
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var cleanUrl = CleanUrl(url);
+            var response = await client.GetAsync(cleanUrl);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// make http request and return xml
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public string GetXML(string url)
+        {
+            client.DefaultRequestHeaders.Add("Accept", "application/xml");
+            var cleanUrl = CleanUrl(url);
+            var response = client.GetAsync(cleanUrl).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        /// <summary>
+        /// make asynchronous http request and return xml
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<string> GetXMLAsync(string url)
+        {
+            client.DefaultRequestHeaders.Add("Accept", "application/xml");
+            var cleanUrl = CleanUrl(url);
+            var response = await client.GetAsync(cleanUrl);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+
+        #region private methods
+
+        private void Init(string baseAddress)
+        {
+            if (string.IsNullOrEmpty(baseAddress))
+                throw new ArgumentException("No base url passed to constructor.");
+
+            //*****************************************clean up base address before sending*************************************//
+            baseAddress = baseAddress.StartsWith("http://") || baseAddress.StartsWith("https://")
+                              ? baseAddress
+                              : "http://" + baseAddress;
+
+            if ((baseAddress.Substring(baseAddress.Length - 1, 1)) != "/")
+                baseAddress += "/";
+
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+            //******************************************************************************************************************//
+        }
+
+        //set up basic authentication
+        private void SetBasicAuth(string username, string password)
+        {
+            var authenticationBytes = Encoding.UTF8.GetBytes(string.Format("{0}:{1}", username, password));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                                                                                       Convert.ToBase64String(
+                                                                                           authenticationBytes));
+        }
+
+        private static string CleanUrl(string url)
+        {
+            return url.StartsWith("/") ? url.Substring(1, url.Length - 1) : url;
+        }
+
+        #endregion
+    }
+}
